c40f1fee4eefbbe2f29397fdcc7407a821e481f1
drean5052023-lgtm <drean505.2023@gmail.com>
2026-07-05 05:24:14 -0500

    Update script.js

--- script/script.js
+++ script/script.js
@@ -23,3 +23,72 @@ document.addEventListener("click",(e)=>{
     }
 
 });
+
+const slides=document.querySelector(".slides");
+
+const imagenes=document.querySelectorAll(".slides img");
+
+const siguiente=document.querySelector(".next");
+
+const anterior=document.querySelector(".prev");
+let indice=0;
+function mostrar(){
+
+    slides.style.transform=`translateX(-${indice*100}%)`;
+
+}
+siguiente.addEventListener("click",()=>{
+
+    indice++;
+
+    if(indice>=imagenes.length){
+
+        indice=0;
+
+    }
+
+    mostrar();
+
+});
+anterior.addEventListener("click",()=>{
+
+    indice--;
+
+    if(indice<0){
+
+        indice=imagenes.length-1;
+
+    }
+
+    mostrar();
+
+});
+setInterval(()=>{
+
+    indice++;
+
+    if(indice>=imagenes.length){
+
+        indice=0;
+
+    }
+
+    mostrar();
+
+},4000);
+let intervalo;
+function iniciar(){
+
+    intervalo=setInterval();
+
+}
+carrusel.addEventListener("mouseenter",()=>{
+
+    clearInterval(intervalo);
+
+});
+carrusel.addEventListener("mouseleave",()=>{
+
+    iniciar();
+
+});
